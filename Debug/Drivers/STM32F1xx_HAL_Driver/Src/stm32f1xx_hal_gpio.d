85f73d93d1e712b99dd9a762bbf731309a5c94c7
Pietro <you@example.com>
2021-08-02 21:27:57 -0300

    commit 1

---
+++ Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.d
@@ -0,0 +1,74 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Core/Inc/stm32f1xx_hal_conf.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/core_cm3.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/cmsis_version.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/cmsis_compiler.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/cmsis_gcc.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Core/Inc/stm32f1xx_hal_conf.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/core_cm3.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/cmsis_version.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+P:/UFMG/Prog.\ Embarcados/Projetos\ Pietro/clockproject/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
